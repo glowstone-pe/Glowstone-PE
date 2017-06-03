071fe5939f3e3afac72c7af4067a97575edb0583
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-06-03 14:33:56 +0200

    add files yeaaa

---
+++ src/pocketmine/inventory/BeaconInventory.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ *
+ *  _____            _               _____           
+ * / ____|          (_)             |  __ \          
+ *| |  __  ___ _ __  _ ___ _   _ ___| |__) | __ ___  
+ *| | |_ |/ _ \ '_ \| / __| | | / __|  ___/ '__/ _ \ 
+ *| |__| |  __/ | | | \__ \ |_| \__ \ |   | | | (_) |
+ * \_____|\___|_| |_|_|___/\__, |___/_|   |_|  \___/ 
+ *                         __/ |                    
+ *                        |___/                     
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author GenisysPro
+ * @link https://github.com/GenisysPro/GenisysPro
+ *
+ *
+*/
+
+namespace pocketmine\inventory;
+
+use pocketmine\tile\Beacon;
+
+class BeaconInventory extends ContainerInventory {
+	
+	public function __construct(Beacon $tile){
+		parent::__construct($tile, InventoryType::get(InventoryType::BEACON));
+	}
+	
+	public function getHolder() {
+		return $this->holder;
+	}
+}
